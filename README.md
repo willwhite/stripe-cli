c8311b05a8e8f8d82774259f308a4af05288e044
Will White <will@mapbox.com>
2013-12-30 14:22:46 -0500

    first

---
+++ README.md
@@ -0,0 +1,28 @@
+Stripe CLI
+==========
+A JavaScript/JSON compatible port of [Stripe::CLI](https://github.com/stripe-contrib/stripe-cli).
+
+**This is a work in progress. Currently only listing is supported.**
+
+Install
+=======
+
+```
+npm install -g stripe-cli
+```
+
+Configuration
+=============
+
+Add your Stripe secret key to a JSON file located at `~/.striperc`:
+
+```
+{
+    "key": "sk_******************"
+}
+```
+
+Usage
+=====
+
+Run `stripe` or see USAGE.txt for usage information.
---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var optimist = require('optimist')
+var argv = optimist.usage(fs.readFileSync('./USAGE.txt', 'utf8'))
+    .argv;
+var env = require('superenv')('stripe');
+var stripe = require('stripe')(env.key);
+
+var resource = argv._[0];
+var command = argv._[1];
+
+if (!resource || !stripe[resource]) {
+    console.error('Invalid resource');
+    optimist.showHelp();
+    process.exit(1);
+}
+
+if (!command || !stripe[resource][command]) {
+    console.error('Invalid command');
+    optimist.showHelp();
+    process.exit(1);
+}
+
+var objects = [];
+(function list(offset) {
+    stripe[resource][command]({
+        count: 100,
+        offset: 100 * offset
+    }, function(err, i) {
+        if (err) throw err;
+        if (i.data.length == 0) return console.log(objects);
+        objects = objects.concat(i.data);
+        list(++offset);
+    });
+})(0);
